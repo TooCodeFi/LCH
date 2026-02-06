e1bc16eedab8c3c06f51db1a07dce4de8acdfc19
TooCodeFi <toouse15@gmail.com>
2026-02-06 12:58:45 +0530

    eee

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-"use client";
-import ExtractForm from "./src/components/ExtractForm";
+import dynamic from "next/dynamic";
 
-export default function page() {
+const ExtractForm = dynamic(() => import("./src/components/ExtractForm"), {
+  ssr: false,
+});
+
+export default function Page() {
   return (
     <div>
       <ExtractForm />
     </div>
   );
 }
+
+// "use client";
+// import ExtractForm from "./src/components/ExtractForm";
+
+// export default function page() {
+//   return (
+//     <div>
+//       <ExtractForm />
+//     </div>
+//   );
+// }
