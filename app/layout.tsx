7fb5d508449574009788345069ec1b0db1c38c99
TooCodeFi <toouse15@gmail.com>
2026-02-06 12:45:52 +0530

    eee

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,36 +23,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import { TourProvider } from "./src/contexts/TourPackageContext";
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {};
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {/* {children} */}
-//         <TourProvider>{children}</TourProvider>
-//       </body>
-//     </html>
-//   );
-// }
