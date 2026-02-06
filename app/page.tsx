9917952172bb28db2cc728f975d77f19c4d61702
TooCodeFi <toouse15@gmail.com>
2026-02-06 12:55:04 +0530

    ddd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-"use client";
-import ExtractForm from "./src/components/ExtractForm";
+import dynamic from "next/dynamic";
 
-export default function page() {
+const ExtractForm = dynamic(() => import("./src/components/ExtractForm"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+export default function Page() {
   return (
     <div>
       <ExtractForm />
