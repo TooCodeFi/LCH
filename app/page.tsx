464ee5a3fc5759cda41290e880ebefdd5d3dd649
TooCodeFi <toouse15@gmail.com>
2026-02-06 12:49:55 +0530

    aaa

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import ExtractForm from "./src/components/ExtractForm";
 export default function page() {
   return (
     <div>
-      {/* <ShowData /> */}
-      {/* <WebScreen /> */}
       <ExtractForm />
     </div>
   );
